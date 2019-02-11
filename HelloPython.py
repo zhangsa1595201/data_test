887fe2a6190611cb795e6cb7b92f300f7edb1eb9
zhangsa1595201 <2269579003@qq.com>
2019-02-11 21:09:13 +0800

    win测试

---
+++ HelloPython.py
@@ -0,0 +1,23 @@
+print(666666666666666666)
+print('hello world')
+print("hello python")
+
+# 单行注释，只在本行起作用
+# print 6766663333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333
+print(777)
+
+'''
+多行注释，作用于单引号/双引号中间的所有内容
+# fdjflksadjfl
+hlkhdsf
+fhdsahflds
+fjldsajfl
+fjlsadjfla
+
+'''
+
+"""
+ifhsldajf
+fkjdspjf
+fjdsoaihfo
+"""
